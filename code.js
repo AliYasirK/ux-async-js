97254fa730190ff8a0190395ff5e00b6105e86ae
Yasir <Yasir.A.Kulane@gmail.com>
2020-08-19 21:50:52 -0500

    testing my finished code

--- code.js
+++ code.js
@@ -1,32 +1,40 @@
-window.onload = function () { // Run this once the page has loaded.
-    // search
-    document.querySelector("#searchButton").addEventListener("click", searchGithub);
-    function searchGithub() {
-        const searchUserText = document.querySelector("#searchUser").value;
+window.onload = function () {
+  // Run this once the page has loaded.
+  // search
+  document
+    .querySelector("#searchButton")
+    .addEventListener("click", searchGithub);
+  function searchGithub() {
+    const searchUserText = document.querySelector("#searchUser").value;
 
-        
-        // Obtain a list of users from the Github API that match searchUserText
-        //  The final result will contain an array under the key 'items'
-        // Pass this array to `renderUserList`
+    // https://api.github.com/search/users?q=searchUserText
 
-        fetch("https://developer.github.com/v3/search/#search-users.") 
-        .then(renderUserList())
-        .catch(renderUserList() 
-        );
+    // fetch the result with searchUserText as paremeter
+    fetch(`https://api.github.com/search/users?q=${searchUserText}`)
+      .then((response) => response.json())
+      .then((data) => renderUserList(data.items));
+
+    // Obtain a list of users from the Github API that match searchUserText
+    //  The final result will contain an array under the key 'items'
+    // Pass this array to `renderUserList`
+  }
+
+  function renderUserList(githubUsers) {
+    let html = "";
+    html += "<ul>";
+    for (let i = 0; i < githubUsers.length; i++) {
+      let githubUser = githubUsers[i];
+      html += "<li>";
+      html += `<strong>${githubUser.login}</strong><code>   -   </code>`;
+      html += `<a target="_blank" href="${githubUser.html_url}">GitHub profile</a>`;
+      html += "</br>";
+      // Add in linked profile avatar image
+      html += `<a target="_blank" href="${githubUser.html_url}"><image height="100px" src="${githubUser.avatar_url}"></a>`;
+      html += "</br>";
+      html += "</li>";
     }
+    html += "</ul>";
 
-    function renderUserList(githubUsers) {
-        let html = "";
-        html += "<ul>";
-        for (let i = 0; i < githubUsers.length; i++) {
-            let githubUser = githubUsers[i];
-            html += "<li>";
-            html += `<strong>${githubUser.login}</strong>`;
-            html += `<a target="_blank" href="${githubUser.html_url}">`;
-            html += "</li>";
-        }
-        html += "</ul>";
-
-        document.querySelector("#resultsContainer").innerHTML = html;
-    }
-}
+    document.querySelector("#resultsContainer").innerHTML = html;
+  }
+};
